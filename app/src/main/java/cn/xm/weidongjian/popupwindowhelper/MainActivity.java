40dc62df13afdbfd002b74bacb8832d357011271
weidongjian <weidongjian@gmail.com>
2015-08-03 13:52:36 +0800

    initial commit

---
+++ app/src/main/java/cn/xm/weidongjian/popupwindowhelper/MainActivity.java
@@ -0,0 +1,51 @@
+package cn.xm.weidongjian.popupwindowhelper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import cn.xm.weidongjian.popuphelper.PopupWindowHelper;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private PopupWindowHelper popupWindowHelper;
+    private View popView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        popView = LayoutInflater.from(this).inflate(R.layout.popupview, null);
+        popupWindowHelper = new PopupWindowHelper(popView);
+
+        popView.findViewById(R.id.button3).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                popupWindowHelper.showAsPopUp(v);
+                break;
+            case R.id.button2:
+                popupWindowHelper.showAsDropDown(v);
+                break;
+            case R.id.button3:
+                popupWindowHelper.dismiss();
+                break;
+            case R.id.button4:
+                popupWindowHelper.showFromTop(v);
+                break;
+            case R.id.button5:
+                popupWindowHelper.showFromBottom(v);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ popuphelper/src/main/java/cn/xm/weidongjian/popuphelper/PopupWindowHelper.java
@@ -0,0 +1,97 @@
+package cn.xm.weidongjian.popuphelper;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+
+/**
+ * Created by Administrator on 2015/8/3.
+ */
+public class PopupWindowHelper {
+
+    private View popupView;
+    private PopupWindow mPopupWindow;
+    private static final int TYPE_WRAP_CONTENT = 0, TYPE_MATCH_PARENT = 1;
+
+    public PopupWindowHelper(View view) {
+        popupView = view;
+    }
+
+    public void showAsDropDown(View anchor) {
+        mPopupWindow.showAsDropDown(anchor);
+    }
+
+    public void showAsDropDown(View anchor, int xoff, int yoff) {
+        initPopupWindow(TYPE_WRAP_CONTENT);
+        mPopupWindow.showAsDropDown(anchor, xoff, yoff);
+    }
+
+    public void showAtLocation(View parent, int gravity, int x, int y) {
+        initPopupWindow(TYPE_WRAP_CONTENT);
+        mPopupWindow.showAtLocation(parent, gravity, x, y);
+    }
+
+    public void dismiss() {
+        if (mPopupWindow.isShowing()){
+            mPopupWindow.dismiss();
+        }
+    }
+
+    public void showAsPopUp(View anchor) {
+        showAsPopUp(anchor, 0, 0);
+    }
+
+    public void showAsPopUp(View anchor, int xoff, int yoff) {
+        initPopupWindow(TYPE_WRAP_CONTENT);
+        mPopupWindow.setAnimationStyle(R.style.AnimationUpPopup);
+        popupView.measure(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        int height = popupView.getMeasuredHeight();
+        int[] location = new int[2];
+        anchor.getLocationInWindow(location);
+        mPopupWindow.showAtLocation(anchor, Gravity.LEFT | Gravity.TOP, location[0] + xoff, location[1] - height + yoff);
+    }
+
+    public void showFromBottom(View anchor) {
+        initPopupWindow(TYPE_MATCH_PARENT);
+        mPopupWindow.setAnimationStyle(R.style.AnimationFromButtom);
+        mPopupWindow.showAtLocation(anchor, Gravity.LEFT | Gravity.BOTTOM, 0, 0);
+    }
+
+    public void showFromTop(View anchor) {
+        initPopupWindow(TYPE_MATCH_PARENT);
+        mPopupWindow.setAnimationStyle(R.style.AnimationFromTop);
+        mPopupWindow.showAtLocation(anchor, Gravity.LEFT | Gravity.TOP, 0, getStatusBarHeight());
+    }
+
+    /**
+     * touch outside dismiss the popupwindow, default is ture
+     * @param isCancelable
+     */
+    public void setCancelable(boolean isCancelable) {
+        if (isCancelable) {
+            mPopupWindow.setOutsideTouchable(true);
+            mPopupWindow.setFocusable(true);
+        }else {
+            mPopupWindow.setOutsideTouchable(false);
+            mPopupWindow.setFocusable(false);
+        }
+    }
+
+    public void initPopupWindow(int type) {
+        if (type == TYPE_WRAP_CONTENT) {
+            mPopupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }else if (type == TYPE_MATCH_PARENT) {
+            mPopupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+        mPopupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        setCancelable(true);
+    }
+
+    private int getStatusBarHeight() {
+        return Math.round(25 * Resources.getSystem().getDisplayMetrics().density);
+    }
+}
